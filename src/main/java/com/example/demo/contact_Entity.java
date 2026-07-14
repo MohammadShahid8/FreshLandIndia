6b6d4a16249b7f1e991456996133d3ab64497af2
Mohammad Shahid <shahid080401@gmail.com>
2026-07-14 23:21:10 +0530

    Update contact_Entity.java

--- src/main/java/com/example/demo/contact_Entity.java
+++ src/main/java/com/example/demo/contact_Entity.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class contact_Entity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String name;
