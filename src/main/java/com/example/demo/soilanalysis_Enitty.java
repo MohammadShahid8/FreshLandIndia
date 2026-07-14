c7886c67802319959a0c8c330d207315a9148f97
Mohammad Shahid <shahid080401@gmail.com>
2026-07-14 23:23:28 +0530

    Update soilanalysis_Enitty.java

--- src/main/java/com/example/demo/soilanalysis_Enitty.java
+++ src/main/java/com/example/demo/soilanalysis_Enitty.java
@@ -12,7 +12,7 @@ public class soilanalysis_Enitty {
 	
     
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String sname;
