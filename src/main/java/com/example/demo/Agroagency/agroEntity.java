a3ff46262f0e01b25526e60a8302afd35d610b84
Mohammad Shahid <shahid080401@gmail.com>
2026-07-14 23:22:03 +0530

    Change ID generation strategy to IDENTITY

--- src/main/java/com/example/demo/Agroagency/agroEntity.java
+++ src/main/java/com/example/demo/Agroagency/agroEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class agroEntity {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	private String name;
